50aac87d37bd251cd402c7b860fcfcad1855d4cd
drr99 <sysy19993027@gmail.com>
2026-04-01 16:56:48 +0900

    리액트로 주소만들기

---
+++ src/data/headerData.js
@@ -0,0 +1,15 @@
+// export const headerData = {
+//   menu: ["신메뉴", "가맹점", "레시피", "디져트", "컬렉션"],
+//   member: ["로그인", "회원가입"],
+// };
+
+export const menuData = [
+  { title: "신메뉴", link: "#imageType" },
+  { title: "가맹점", link: "#imgTextType" },
+  { title: "레시피", link: "#cardType" },
+  { title: "디져트", link: "#bannerType" },
+  { title: "컬렉션", link: "#textType" },
+  { title: "회사정보", link: "#footerType" },
+];
+
+export const memberData = ["로그인", "회원가입"];
